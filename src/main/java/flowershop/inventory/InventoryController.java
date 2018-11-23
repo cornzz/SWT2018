de487215e2d2f96fa206e5744e5770ba0a34661d
friedrichbethke <friedrichbethke@live.de>
2018-11-23 09:11:56 +0100

    Add small polish

--- src/main/java/flowershop/inventory/InventoryController.java
+++ src/main/java/flowershop/inventory/InventoryController.java
@@ -1,11 +1,19 @@
 package flowershop.inventory;
 
+import flowershop.catalog.Item;
 import org.salespointframework.inventory.Inventory;
 import org.salespointframework.inventory.InventoryItem;
+import org.salespointframework.inventory.InventoryItemIdentifier;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.salespointframework.quantity.Quantity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class InventoryController {
@@ -25,4 +33,14 @@ public class InventoryController {
 
 		return "inventory";
 	}
+
+	@PostMapping("/deficit/{id}")
+	public String deficit(@PathVariable InventoryItemIdentifier id){
+		inventory.findById(id).get().decreaseQuantity(Quantity.of(1));
+		inventory.save(inventory.findById(id).get());
+
+
+		return "redirect:/";
+	}
+
 }
