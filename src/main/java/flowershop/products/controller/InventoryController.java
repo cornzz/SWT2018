bd20418a5d7e5573da2976be9526d9f511b5e78d
friedrichbethke <friedrichbethke@live.de>
2018-11-25 14:15:10 +0100

    Add missing basic functionality

---
+++ src/main/java/flowershop/products/controller/InventoryController.java
@@ -0,0 +1,59 @@
+package flowershop.products.controller;
+
+
+import flowershop.products.FlowerShopItem;
+import flowershop.products.FlowerShopItemCatalog;
+import org.javamoney.moneta.Money;
+import org.salespointframework.inventory.Inventory;
+import org.salespointframework.inventory.InventoryItem;
+import org.salespointframework.inventory.InventoryItemIdentifier;
+import org.salespointframework.quantity.Quantity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import static org.salespointframework.core.Currencies.EURO;
+
+@Controller
+public class InventoryController {
+
+	private final Inventory<InventoryItem> inventory;
+	private final FlowerShopItemCatalog itemCatalog;
+
+	InventoryController(Inventory<InventoryItem> inventory, FlowerShopItemCatalog itemCatalog) {
+		this.itemCatalog = itemCatalog;
+		this.inventory = inventory;
+	}
+
+	@GetMapping("/products/items/stock")
+	public String inventory(Model model) {
+		model.addAttribute("inventory", inventory.findAll());
+
+		return "inventory";
+	}
+
+	// Do a Deficit
+	@RequestMapping("/products/items/stock/deficit/{id}")
+	public String deficit(@PathVariable InventoryItemIdentifier id, @RequestParam int deficit){
+		inventory.findById(id).get().decreaseQuantity(Quantity.of(deficit));
+		inventory.save(inventory.findById(id).get());
+
+		return "redirect:/products/items/stock";
+	}
+
+
+	@GetMapping("/products/items/stock/add")
+	public String add(Model model){
+		return "inventory_add";
+	}
+
+	// Add a new InventoryItem
+	@PostMapping("/products/items/stock/add")
+	public String add(String name, int price, int amount, String description){
+		FlowerShopItem item = new FlowerShopItem(name,Money.of(price,EURO), description);
+		itemCatalog.save(item);
+		inventory.save(new InventoryItem(item,Quantity.of(amount)));
+
+		return "redirect:/products/items/stock";
+	}
+}
---
+++ src/main/java/flowershop/products/controller/ServiceController.java
@@ -0,0 +1,41 @@
+package flowershop.products.controller;
+
+
+import flowershop.products.FlowerShopService;
+import flowershop.products.FlowerShopServiceCatalog;
+import org.javamoney.moneta.Money;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import static org.salespointframework.core.Currencies.EURO;
+
+@Controller
+public class ServiceController {
+
+	private final FlowerShopServiceCatalog serviceCatalog;
+
+	ServiceController(FlowerShopServiceCatalog serviceCatalog){
+		this.serviceCatalog = serviceCatalog;
+	}
+
+	@GetMapping("/products/services")
+	public String services(Model model) {
+		model.addAttribute("services", serviceCatalog.findAll());
+
+		return "services";
+	}
+
+	@GetMapping("/products/services/add")
+	public String add(Model model){
+		return "service_add";
+	}
+
+	// Add a new Service
+	@PostMapping("/products/services/add")
+	public String add(String name, int price, String description){
+		FlowerShopService service = new FlowerShopService(name, Money.of(price,EURO), description);
+		serviceCatalog.save(service);
+		return "redirect:/products/services";
+	}
+}
