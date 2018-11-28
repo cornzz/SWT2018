bba57e4a90ccfef3aca9d01838807346316e9ee9
cornzz <39997278+cornzz@users.noreply.github.com>
2018-11-28 01:34:23 +0100

    Add change password dialog #88 (#105)

--- src/main/java/flowershop/user/UserController.java
+++ src/main/java/flowershop/user/UserController.java
@@ -21,107 +21,107 @@ import java.util.Optional;
 @Controller
 public class UserController {
 
-		private final UserManagement userManagement;
-		private final AuthenticationManager authenticationManager;
+	private final UserManagement userManagement;
+	private final AuthenticationManager authenticationManager;
 
-		public UserController(UserManagement userManagement, AuthenticationManager authenticationManager) {
-				this.userManagement = userManagement;
-				this.authenticationManager = authenticationManager;
+	public UserController(UserManagement userManagement, AuthenticationManager authenticationManager) {
+		this.userManagement = userManagement;
+		this.authenticationManager = authenticationManager;
+	}
+
+
+	@GetMapping("/login")
+	String login(@LoggedIn Optional<UserAccount> loggedIn) {
+		return loggedIn.isPresent() ? "redirect:/products" : "login";
+	}
+
+	@GetMapping("/register")
+	String register(Model model, UserDataTransferObject form, @LoggedIn Optional<UserAccount> loggedIn) {
+		if (loggedIn.isPresent()) {
+			return "redirect:/account";
 		}
 
+		model.addAttribute("form", form);
+		return "register";
+	}
 
-		@GetMapping("/login")
-		String login(@LoggedIn Optional<UserAccount> loggedIn) {
-			return loggedIn.isPresent() ? "redirect:/products" : "login";
+	@PostMapping("/register")
+	ModelAndView registerNew(@ModelAttribute("form") @Validated(UserDataTransferObject.RegistrationProcess.class) UserDataTransferObject form, BindingResult result,
+							 HttpServletRequest request, @LoggedIn Optional<UserAccount> loggedIn) {
+		if (loggedIn.isPresent()) {
+			return new ModelAndView("redirect:/account", "form", null);
+		}
+		if (result.hasErrors()) {
+			return new ModelAndView("register", "form", form);
 		}
 
-		@GetMapping("/register")
-		String register(Model model, UserDataTransferObject form, @LoggedIn Optional<UserAccount> loggedIn) {
-				if (loggedIn.isPresent()) {
-						return "redirect:/account";
-				}
-
-				model.addAttribute("form", form);
-				return "register";
+		userManagement.createUser(form);
+		// Authenticate before redirecting
+		try {
+			String username = (form.getFirstName() + form.getLastName()).toLowerCase();
+			request.login(username, form.getPassword());
+		} catch (ServletException e) {
 		}
 
-		@PostMapping("/register")
-		ModelAndView registerNew(@ModelAttribute("form") @Validated(UserDataTransferObject.RegistrationProcess.class) UserDataTransferObject form, BindingResult result,
-				HttpServletRequest request, @LoggedIn Optional<UserAccount> loggedIn) {
-				if (loggedIn.isPresent()) {
-						return new ModelAndView("redirect:/account", "form", null);
-				}
-				if (result.hasErrors()) {
-						return new ModelAndView("register", "form", form);
-				}
+		return new ModelAndView("forward:/", "message", String.format("Thank you for signing up, %s!", form.getFirstName()));
+	}
 
-				userManagement.createUser(form);
-				// Authenticate before redirecting
-				try {
-						String username = (form.getFirstName() + form.getLastName()).toLowerCase();
-						request.login(username, form.getPassword());
-				} catch (ServletException e) {
-				}
+	@GetMapping("/account")
+	@PreAuthorize("isAuthenticated()")
+	String account(Model model, UserDataTransferObject form, @LoggedIn Optional<UserAccount> loggedIn) {
+		populateForm(form, loggedIn.get());
+		model.addAttribute("form", form);
 
-				return new ModelAndView("forward:/", "message", String.format("Thank you for signing up, %s!", form.getFirstName()));
+		return "account";
+	}
+
+	@PostMapping("/account")
+	@PreAuthorize("isAuthenticated()")
+	ModelAndView updateAccount(Model model, @ModelAttribute("form") @Validated(UserDataTransferObject.UpdateAccProcess.class) UserDataTransferObject form, BindingResult result,
+							   @LoggedIn Optional<UserAccount> loggedIn) {
+		if (result.hasErrors()) {
+			return new ModelAndView("account", "form", form);
 		}
 
-		@GetMapping("/account")
-		@PreAuthorize("isAuthenticated()")
-		String account(Model model, UserDataTransferObject form, @LoggedIn Optional<UserAccount> loggedIn) {
-				populateForm(form, loggedIn.get());
-				model.addAttribute("form", form);
+		userManagement.modifyUser(form, loggedIn.get());
 
-				return "account";
+		return new ModelAndView("redirect:/account?success", "form", form);
+	}
+
+	@GetMapping("/account/changepass")
+	@PreAuthorize("isAuthenticated()")
+	ModelAndView redirectAccount(Model model, UserDataTransferObject form) {
+		return new ModelAndView("account_changepass", "form", form);
+	}
+
+	@PostMapping("/account/changepass")
+	@PreAuthorize("isAuthenticated()")
+	ModelAndView changePassword(Model model, @ModelAttribute("form") @Validated(UserDataTransferObject.ChangePassProcess.class) UserDataTransferObject form, BindingResult result,
+								@LoggedIn Optional<UserAccount> loggedIn) {
+		if (result.hasErrors()) {
+			populateForm(form, loggedIn.get());
+			return new ModelAndView("account_changepass", "form", form);
 		}
 
-		@PostMapping("/account")
-		@PreAuthorize("isAuthenticated()")
-		ModelAndView updateAccount(Model model, @ModelAttribute("form") @Validated(UserDataTransferObject.UpdateAccProcess.class) UserDataTransferObject form, BindingResult result,
-				@LoggedIn Optional<UserAccount> loggedIn) {
-				if (result.hasErrors()) {
-						return new ModelAndView("account", "form", form);
-				}
+		userManagement.changePass(form, loggedIn.get());
 
-				userManagement.modifyUser(form, loggedIn.get());
+		return new ModelAndView("redirect:/account?success", "form", form);
+	}
 
-				return new ModelAndView("redirect:/account?success", "form", form);
-		}
+	@GetMapping("/users")
+	@PreAuthorize("hasRole('ROLE_BOSS')")
+	String users(Model model) {
+		model.addAttribute("userList", userManagement.findAll());
 
-		@GetMapping("/account/changepass")
-		@PreAuthorize("isAuthenticated()")
-		String redirectAccount() {
-				return "redirect:/account";
-		}
+		return "users";
+	}
 
-		@PostMapping("/account/changepass")
-		@PreAuthorize("isAuthenticated()")
-		ModelAndView changePassword(Model model, @ModelAttribute("form") @Validated(UserDataTransferObject.ChangePassProcess.class) UserDataTransferObject form, BindingResult result,
-				@LoggedIn Optional<UserAccount> loggedIn) {
-				if (result.hasErrors()) {
-						populateForm(form, loggedIn.get());
-						return new ModelAndView("account", "form", form);
-				}
-
-				userManagement.changePass(form, loggedIn.get());
-
-				return new ModelAndView("redirect:/account?success", "form", form);
-		}
-
-		@GetMapping("/users")
-		@PreAuthorize("hasRole('ROLE_BOSS')")
-		String users(Model model) {
-				model.addAttribute("userList", userManagement.findAll());
-
-				return "users";
-		}
-
-		private void populateForm(UserDataTransferObject form, @LoggedIn UserAccount loggedIn) {
-				User user = userManagement.findByAccount(loggedIn).get();
-				form.setFirstName(loggedIn.getFirstname());
-				form.setLastName(loggedIn.getLastname());
-				form.setEmail(loggedIn.getEmail());
-				form.setPhone(user.getPhone());
-		}
+	private void populateForm(UserDataTransferObject form, @LoggedIn UserAccount loggedIn) {
+		User user = userManagement.findByAccount(loggedIn).get();
+		form.setFirstName(loggedIn.getFirstname());
+		form.setLastName(loggedIn.getLastname());
+		form.setEmail(loggedIn.getEmail());
+		form.setPhone(user.getPhone());
+	}
 
 }
